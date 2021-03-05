a20ab2b8d7df393ab48e46305a50b27194e8ee45
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-03-05 22:35:10 +0300

    docs: updates on workflows
    Added information on documentation

--- tests/css.test.ts
+++ tests/css.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals'
 
 import { css } from '../utils/getCss'
-import { LayoutPattern } from "../typings/types";
+import { LayoutPattern } from '../typings/types'
 
 describe('Testing CSS style', () => {
     test('Style CSS SVG', () => {
